708034dffb64446bd60e7cacac7a833c4f8aa2a6
Dalimil Hajek <dalimilhajek@gmail.com>
2016-11-05 15:13:59 +0000

    Add Google Directions API to preprocess tram stop combinations

--- tools/directions.js
+++ tools/directions.js
@@ -1,17 +1,45 @@
 /** Sample code using the 'request' module to make server-side http requests */
+"use strict";
+
 const request = require('request');
 
 const origin = "75+9th+Ave+New+York,+NY";
 const destination = "MetLife+Stadium+1+MetLife+Stadium+Dr+East+Rutherford,+NJ+07073";
 const key = "AIzaSyCNtpdJ-cx0367iMVm1oQsioj8ItVMkkww";
 
-/* GET */
-request(`https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&mode=transit&arrival_time=1478520000&key=${key}`,
-	function(error, response, body) {
-		// Callback function
-		if (!error && response.statusCode == 200) {
-			console.log(body); // Show data
-		}
-	}
-);
+const stopPairs = [{ from: 'a', to: 'b' }];
+const routes = {};
+let pairsToDo = stopPairs.length;
 
+stopPairs.forEach(function(pair, ind) {
+	setTimeout(function() { getDirections(pair); }, ind * 200);
+});
+
+function getDirections(pair) {
+	let from = pair.from;
+	let to = pair.to;
+
+	request(`https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&mode=transit&arrival_time=1478520000&key=${key}`,
+		function(error, response, body) {
+			// Callback function
+			if (!error && response.statusCode == 200) {
+				let data = JSON.parse(body);
+				let steps = data.routes[0].legs[0].steps;
+
+				let routeKey = from + ";" + to;
+				routes[routeKey] = steps.map(step => { return { from: step.start_location, to: step.end_location }});
+				console.log(routeKey);
+				
+
+				pairsToDo -= 1;
+				if (pairsToDo <= 0) {
+					finish();
+				}
+			}
+		}
+	);
+}
+
+function finish() {
+	console.log(JSON.stringify(routes, null, 2));
+}
--- tools/process.js
+++ tools/process.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 let lines = fs.readFileSync('tfl-p6-sep-data.csv').toString().split("\n");
 
 let trams = {};
+let stopNames = {};
 
 lines.forEach(line => {
 	let attrs = line.split(",");
@@ -21,13 +22,52 @@ lines.forEach(line => {
 	let percentSeatLoad = attrs[11];
 	let percentLoad = attrs[12];
 	//date,stopCode,stopName,stopType,time,waitTime,line,tramId,add,rem,load,seatLoadPercent,loadPercent,...
+	//Date,Code,Stop,Stop Type,Time,R Length,Line,Tram,Ins,Outs,Load,Seat. load,%Load,I,V,P
 	//console.log(time);
 
+	var stopData = { time: time, stop: stopCode, start: (stopType == 'A') };
 	if (trams[tramId] == undefined) {
-		trams[tramId] = [{ time: time, stop: stopCode }];
+		trams[tramId] = [stopData];
+	} else {
+		trams[tramId].push(stopData);
 	}
+	if (stopName == '* UNKNOWN *') {
+		console.log(time, stopType);
+	}
+	stopNames[stopName] = 1;
+});
+
+// console.log(Object.keys(trams).map(xKey => { let x = trams[xKey]; return x[x.length-1] }));
+
+let tramJourneyIndx = {};
+
+
+lines.forEach(line => {
+	let attrs = line.split(",");
+	let stopCode = attrs[1];
+	let stopName = attrs[2];
+	let stopType = attrs[3];
+	let time = attrs[4];
+	let waitTime = attrs[5];
+	let tramLine = attrs[6];
+	let tramId = attrs[7];
+	let peopleIn = attrs[8];
+	let peopleOut = attrs[9];
+	let peopleLoad = attrs[10];
+	let percentSeatLoad = attrs[11];
+	let percentLoad = attrs[12];
+
+	tramJourneyIndx[tramId] = tramJourneyIndx[tramId] + 1 || 0;
 	
 });
 
 
-//Date,Code,Stop,Stop Type,Time,R Length,Line,Tram,Ins,Outs,Load,Seat. load,%Load,I,V,P
+function timeToSeconds(formatedTime) {
+	let s = formatedTime.split(":");
+	let num = parseInt(s[0]) * 3600 + parseInt(s[1]) * 60 + parseInt[2];
+	return num;
+}
+
+
+console.log(Object.keys(stopNames));
+
