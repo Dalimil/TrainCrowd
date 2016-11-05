2f20aea44ab2a73d8c93e692855df7c86e5ab8dc
Dalimil Hajek <dalimilhajek@gmail.com>
2016-11-05 23:51:16 +0000

    Add tram layer and marker movement

--- public/js/main.js
+++ public/js/main.js
@@ -42,12 +42,23 @@ var stations = [
 		{"station":"Addiscombe","lat":51.3798767,"lon":-0.0754758}
 	];
 
+var stationsIndex = {};
+stations.forEach(station => {
+	stationsIndex[station.station] = { lat: station.lat, lng: station.lon };
+});
+
 var map;
 var tramLayer;
+var $legend;
+var ik = {
+    className: 'my-icon icon-dc', // class name to style
+    html: '&#9733;', // add content inside the marker
+    iconSize: null // size of icon, use null to set the size in CSS
+  };
 
 $(function() {
 	$("#map").css({
-		height: $("body").height() * 0.9
+		height: $("body").height() * 1.0
 	});
 
 	L.mapbox.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpbG10dnA3NzY3OTZ0dmtwejN2ZnUycjYifQ.1W5oTOnWXQ9R1w8u3Oo1yA';
@@ -87,42 +98,57 @@ $(function() {
 
 	// Add marker color, symbol, and size to library GeoJSON
 	for (var j = 0; j < libraries.features.length; j++) {
-		libraries.features[j].properties['marker-color'] = '#4169E1';
-		libraries.features[j].properties['marker-symbol'] = 'rail';
-		libraries.features[j].properties['marker-size'] = 'small';
+		//libraries.features[j].properties['marker-color'] = '#4169E1';
+		//libraries.features[j].properties['marker-symbol'] = 'rail';
+		//libraries.features[j].properties['marker-size'] = 'small';
 	}
 
-	window.libraryLayer = L.mapbox.featureLayer(libraries).addTo(map);
-	var stationRouteLayer = L.mapbox.featureLayer(routeLayerData, {}).addTo(map); //  style: { color: '#555' } 
 
-	console.log(stationRouteLayer);
 
-	map.fitBounds(stationRouteLayer.getBounds());
+	var libraryLayer = L.mapbox.featureLayer(libraries).addTo(map);
+	//var stationRouteLayer = L.mapbox.featureLayer(routeLayerData, {}).addTo(map); //  style: { color: '#555' } 
+
+	libraryLayer.on('layeradd', function(e) {
+	  var marker = e.layer,
+	    feature = marker.feature;
+	  marker.setIcon(L.divIcon(ik));
+	});
+	libraryLayer.setGeoJSON(libraries);
+
+	//console.log(stationRouteLayer);
+
+	map.fitBounds(libraryLayer.getBounds());
 	map.on('click', function(e) { console.log("click ", e.latlng); });
 
+	startAnimation();
 
 });
 
 
 function startAnimation() {
 	$.get("static/js/tfl-p6-sep-data.csv", function(data) {
-		var lines = data.split("\n");
+		lines = data.split("\n");
 		console.log(data.substr(0, 10));
-		process(lines);
+		process();
 	});
 }
 
+let lines;
+let trams = {};
+let tramJourneyIndx = {};
+let activeTrams = {};
+let tramLayerData;
+let currentLineIndex = 0;
+let animationInterval;
 
-function process(lines) {
-	let trams = {};
-	let tramJourneyIndx = {};
+function process() {
 
 	lines.forEach(line => {
 		let attrs = line.split(",");
 		let stopCode = attrs[1];
 		let stopName = attrs[2].replace(/\'/g, "");
 		let stopType = attrs[3];
-		let time = attrs[4];
+		let time = timeToSeconds(attrs[4]);
 		let waitTime = attrs[5];
 		let tramLine = attrs[6];
 		let tramId = attrs[7];
@@ -144,68 +170,120 @@ function process(lines) {
 		
 	});
 
-	// console.log(Object.keys(trams).map(xKey => { let x = trams[xKey]; return x[x.length-1] }));
-	var activeTrams = {};
-
-	var tramLayerData = {
+	tramLayerData = {
 		type: 'FeatureCollection',
-		features: [
-			{
-				type: "Feature",
-			    properties: {
-			    	title: 'Tram',
-			    	'marker-color': "#E55",
-			    	'marker-size': "small"
-			    },
-			    geometry: {
-			        type: "Point",
-			   		coordinates: [-0.1009668, 51.558227]
-			   	}
-			}
-		]
+		features: [	]
 	};
 
 	tramLayer = L.mapbox.featureLayer(tramLayerData).addTo(map);
 
-	tramLayerData.features.push(			{
-				type: "Feature",
-			    properties: {
-			    	title: 'Tram2',
-			    	'marker-color': "#E55",
-			    	'marker-size': "small"
-			    },
-			    geometry: {
-			        type: "Point",
-			   		coordinates: [-0.1059668, 51.3758227]
-			   	}
-			});
+	var legendControl = L.mapbox.legendControl();
+	legendControl.addLegend('<div class="legend"></div>');
+	map.addControl(legendControl);
 
-	lines.forEach(line => {
-		let attrs = line.split(",");
-		let stopCode = attrs[1];
-		let stopName = attrs[2].replace(/\'/g, "");
-		let stopType = attrs[3];
-		let time = attrs[4];
-		let waitTime = attrs[5];
-		let tramLine = attrs[6];
-		let tramId = attrs[7];
-		let peopleIn = attrs[8];
-		let peopleOut = attrs[9];
-		let peopleLoad = attrs[10];
-		let percentSeatLoad = attrs[11];
-		let percentLoad = attrs[12];
-
-		if (activeTrams[tramId] == undefined) {
-			// create tram
-		} else {
-			// update layer
-		}
-
-		tramJourneyIndx[tramId] = tramJourneyIndx[tramId] + 1 || 0;
-
-	});
+	animationInterval = setInterval(tick, 170);
+	$legend = $(".legend");
 
 }
+
+let redrawTime;
+let currentTime;
+
+function tick() {
+	let line = lines[currentLineIndex];
+
+	let attrs = line.split(",");
+	let stopCode = attrs[1];
+	let stopName = attrs[2].replace(/\'/g, "");
+	let stopType = attrs[3];
+	let time = timeToSeconds(attrs[4]);
+	let waitTime = timeToSeconds(attrs[5]);
+	let tramLine = attrs[6];
+	let tramId = attrs[7];
+	let peopleIn = attrs[8];
+	let peopleOut = attrs[9];
+	let peopleLoad = attrs[10];
+	let percentSeatLoad = attrs[11];
+	let percentLoad = attrs[12];
+
+	//console.log(currentTime, time);
+	if (currentTime == undefined) {
+		currentTime = time;
+		redrawTime = time + 5;
+	}
+
+	if (redrawTime + 5 < time) {
+		redrawTime += 5;
+		// just redraw
+		currentTime = redrawTime;
+	} else {
+		currentTime = time;
+		// process this tram event and move on
+		currentLineIndex += 1;
+		console.log(currentLineIndex / lines.length);
+		if (currentLineIndex > lines.length) {
+			clearInterval(animationInterval);
+		}
+
+		//console.log(stopName, time, waitTime);
+
+		delete activeTrams[tramId];
+
+		tramJourneyIndx[tramId] = tramJourneyIndx[tramId] + 1 || 1;
+
+		if (stopName != '*DEPOT*') { // (stopType != 'B') {
+
+			let stationLatlng = stationsIndex[stopName];
+			let tramsJourneyStop = trams[tramId][tramJourneyIndx[tramId]];
+			//console.log(tramsJourneyStop);
+			let toStop = stationsIndex[tramsJourneyStop.stop];
+
+			activeTrams[tramId] = {
+				from: { lat: stationLatlng.lat, lng: stationLatlng.lng, time: time + waitTime },
+				to: { lat: toStop.lat, lng: toStop.lng, time: tramsJourneyStop.time },
+				properties: {
+			    	title: "Line " + tramLine + " - " + tramId,
+			    	description: "Load " + peopleLoad + " " + percentLoad + " " + percentSeatLoad,
+			    	'marker-color': (percentLoad > 10) ? "#C33" : (percentLoad > 5) ? "#CC3" : "#3C3",
+			    	'marker-size': "small"
+			    }
+			};
+		}
+	}
+
+	$legend.text(timeFromSeconds(currentTime);
+		
+	//console.log(activeTrams);
+	var newTramLayerData = Object.keys(activeTrams).map(activeTramKey => {
+		let tram = activeTrams[activeTramKey];
+		let coordinates;
+		if (tram.from.time >= currentTime) {
+			coordinates = [tram.from.lng, tram.from.lat];
+		} else {
+			// between
+			let percent = (currentTime - tram.from.time) / (tram.to.time - tram.from.time);
+			//console.log("percent", percent);
+			let movement = [(tram.to.lng - tram.from.lng) * percent, (tram.to.lat - tram.from.lat) * percent];
+			coordinates = [tram.from.lng + movement[0], tram.from.lat + movement[1]];
+		}
+
+		// create tram
+		var tramJson = {
+			type: "Feature",
+		    properties: tram.properties,
+		    geometry: {
+		        type: "Point",
+		   		coordinates: coordinates
+		   	}
+		};
+		//console.log(tramJson);
+		return tramJson;
+	});
+
+	tramLayerData = newTramLayerData;
+	tramLayer.setGeoJSON(tramLayerData);
+}
+
 
 
 function isStopNameValid(name) {
@@ -213,9 +291,22 @@ function isStopNameValid(name) {
 }
 
 function timeToSeconds(formatedTime) {
-	let s = formatedTime.split(":");
-	let num = parseInt(s[0]) * 3600 + parseInt(s[1]) * 60 + parseInt[2];
-	return num;
+	let s = formatedTime.toString().split(":");
+	let num = parseInt(s[0]) * 3600 + parseInt(s[1]) * 60 + parseInt(s[2]);
+	return num || 0;
 }
+
+function timeFromSeconds(timeSeconds) {
+	let seconds = timeSeconds % 60;
+	timeSeconds = Math.round(timeSeconds / 60);
+
+	let minutes = timeSeconds % 60;
+	timeSeconds = Math.round(timeSeconds / 60);
+
+	let hours = Math.round(timeSeconds);
+
+	return hours + ":" + minutes;
+}
+
 
 
