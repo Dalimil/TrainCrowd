ca0c6920a5c11f42918f572ebc5c2e928d757c2f
Dalimil Hajek <dalimilhajek@gmail.com>
2016-11-05 13:22:06 +0000

    Add turf library

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,11 @@
 $(function() {
 	$("#map").css({
-		height: $("body").height() * 0.8
+		height: $("body").height() * 0.9
 	});
 
 	L.mapbox.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpbG10dnA3NzY3OTZ0dmtwejN2ZnUycjYifQ.1W5oTOnWXQ9R1w8u3Oo1yA';
 
-	var map = L.mapbox.map('map', 'mapbox.dark').setView([40, -74.50], 9);
-});
+	var map = L.mapbox.map('map', 'mapbox.dark').setView([38.05, -84.5], 12);
 
+
+});
---
+++ tools/directions.js
@@ -0,0 +1,16 @@
+/** Sample code using the 'request' module to make server-side http requests */
+const request = require('request');
+
+const origin = "75+9th+Ave+New+York,+NY";
+const destination = "MetLife+Stadium+1+MetLife+Stadium+Dr+East+Rutherford,+NJ+07073";
+
+/* GET */
+request(`https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&mode=transit&arrival_time=1478520000&key=${key}`,
+	function(error, response, body) {
+		// Callback function
+		if (!error && response.statusCode == 200) {
+			console.log(body); // Show the HTML
+		}
+	}
+);
+
